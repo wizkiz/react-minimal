3f3054c475a0822d5262e47d7774d2b9692dcdfb
wizkiz <szy.majorek@gmail.com>
2019-10-14 10:48:16 +0200

    Production bundle analysis

--- src/AppTitle.jsx
+++ src/AppTitle.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 
 const AppTitle = () => (
-  <h1>does it work?</h1>  
+  <h1>
+    <p>Bundle size: 57 bytes, Load time of the bundle: 45 ms, Last commit SHA1: 71550f0b84df81290fc2cd93a63bd71233064347</p>
+  </h1> 
 )
 
 export default AppTitle
--- src/main.js
+++ src/main.js
@@ -3,4 +3,5 @@ import { render } from 'react-dom'
 import MyApp from './App'
 import AppTitle from './AppTitle'
 
+console.log("XD");
 render(<MyApp />, document.getElementById('reactinjectedhere'))
